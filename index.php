94512593bf456a15713559813a220ff5cc77602a
xodnd0507 <24457545yong@gmail.com>
2024-04-03 00:18:57 +0900

    test : Test php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>로그인 및 회원가입 페이지</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <button id="login">로그인</button>
+    <button id="join">회원가입</button>
+
+    <form action="login.php" method="post">
+        <input type="text" name="userid" id="userid" require>
+        <input type="password" name="userpw" id="userpw" require>
+        <input type="submit" value="로그인" id="loginBtn">
+        <input type="reset" value="취소">
+    </form>
+
+    <script src="jquery-1.12.3.js"></script>
+    <script src="index.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+    $id = $_POST["userid"];
+    $pw = $_POST["userpw"];
+
+    if($id == "admin" && $pw == "1234"){
+        $_SESSION["userid"] = $id;
+        $_SESSION["userpw"] = $pw;
+
+        echo "
+                <script>
+                    alert('관리자로 로그인 되었습니다.');
+                    location.href='reservation.html'
+                </script>";
+    }elseif($id == "manager" && $pw == "1234"){
+        $_SESSION["userid"] = $id;
+        $_SESSION["userpw"] = $pw;
+
+        echo "
+                <script>
+                    alert('담당자로 로그인 되었습니다.');
+                    location.href='reservation.html'
+                </script>";
+    }else{
+        $_SESSION["userid"] = $id;
+        $_SESSION["userpw"] = $pw;
+
+        echo "
+                <script>
+                    alert('일반회원으로 로그인 되었습니다.');
+                    location.href='reservation.html'
+                </script>";
+    }
+?>
+
